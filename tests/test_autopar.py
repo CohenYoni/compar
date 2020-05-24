37d65830eaa5f96aee1ac79fea60fc6538310db4
May Hagbi <mayhagbi@gmail.com>
2020-05-24 17:41:28 +0300

    Update test class for class Autopar.

--- tests/test_autopar.py
+++ tests/test_autopar.py
@@ -6,7 +6,6 @@ import exceptions as e
 class TestClassAutopar(unittest.TestCase):
     def setUp(self):
         self.autopar_mock = Autopar(version='1')
-        self.input_file_directory = "test_autopar"
 
     def test_autopar_instance_is_not_none(self):
         self.assertIsNotNone(self.autopar_mock)
