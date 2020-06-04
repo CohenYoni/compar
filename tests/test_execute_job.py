7c5d2f2dcf9a593bdd4b2b0f4c91a8b831b07f5c
Yoel Vaizman <yoelv92@gmail.com>
2020-06-04 16:54:54 +0300

    finish testing execute_job.py

--- tests/test_execute_job.py
+++ tests/test_execute_job.py
@@ -6,14 +6,16 @@ from threading import RLock
 from globals import ComparMode, ComparConfig, CombinatorConfig
 import os
 from parameters import Parameters
-from unittest.mock import patch
-
+from unittest.mock import patch, MagicMock
+from unittest.mock import Mock
+import shutil
 
 class TestExecuteJob(unittest.TestCase):
 
     def setUp(self):
         self.input_file_directory = "unit_test_execute_job"
         self.file_name = "unit_test_execute_job.c"
+
         self.file_output = "unit_test_execute_job.output.c"
         self.file_relative_path = os.path.join(self.input_file_directory, self.file_name)
         self.combination_id = "123"
@@ -29,10 +31,12 @@ class TestExecuteJob(unittest.TestCase):
         self.loop1_serial_run_time = "12.2"
 
         self.num_of_loops_in_files = {}
-        self.db = "DB object"#need Mock
+        self.db = Mock()#"DB object"#need Mock
+
         self.db_lock = RLock()
+
         self.serial_run_time = {(self.file_relative_path, self.loop1_label): self.loop1_serial_run_time}  #{(<file_id_by_rel_path>, <loop_label>) : <run_time>, ... }
-        self.relative_c_file_list = [{"file_name": self.file_name, "file_relative_path": self.file_relative_path }]
+        self.relative_c_file_list = [{"file_name": self.file_name, "file_relative_path": self.file_relative_path}]
         self.slurm_partition = ComparConfig.DEFAULT_SLURM_PARTITION
         self.test_file_path = os.path.join(self.input_file_directory, self.file_output)
 
@@ -63,7 +67,57 @@ class TestExecuteJob(unittest.TestCase):
         self.assertEqual(float(self.loop1_serial_run_time) / float(self.ej.job.get_loop_run_time_in_file_results(self.file_relative_path, self.loop1_label)),
                          float(self.ej.job.get_loop_speedup_in_file_results(self.file_relative_path, self.loop1_label)))
 
+    def test_update_dead_code_files(self):
+        dead_file_name = "unit_test_execute_job_dead_file.c" # not in self.relative_c_file_list
+        self.relative_c_file_list.append({"file_name": dead_file_name,
+                                          "file_relative_path":  os.path.join(self.input_file_directory,
+                                                                              dead_file_name)})
+        self.ej.update_dead_code_files()
+        job_results = self.ej.job.get_job_results()['run_time_results']
+        self.assertTrue(job_results[1]['dead_code_file'])
 
+
+    def test_save_successful_job(self):
+        self.db.insert_new_combination_results('{"key": "value"}')
+        self.db.insert_new_combination_results.return_value = True
+        self.ej.save_successful_job()
+        self.db.insert_new_combination_results.assert_called_with(self.ej.job.get_job_results())
+
+    def test_save_combination_as_failure(self):
+        self.db.insert_new_combination_results('{"key": "value"}')
+        self.db.insert_new_combination_results.return_value = True
+        self.ej.save_combination_as_failure("error_msg")
+        self.db.insert_new_combination_results.assert_called_with({'_id': self.combination_id, 'error': 'error_msg'})
+
+
+    def test_run(self):
+        vals = {'sbatch  -o unit_test_execute_job/unit_test_execute_job.log unit_test_execute_job/batch_job.sh unit_test_execute_job/unit_test_execute_job.x': ('stdout', 'stderr', 'ret_code'),
+                'squeue': 0}
+
+        def side_effect(*args):
+            return vals[args]
+
+        user_slurm_parameters = []
+        batch_dir_file_path = "unit_test_execute_job"
+        if not os.path.exists(batch_dir_file_path):
+            os.mkdir(batch_dir_file_path)
+
+        with patch('execute_job.run_subprocess') as run_process_mock:
+            run_process_mock.side_effect = [('stdout', 'stderr', 'ret_code')]
+            #run_process_mock.return_value = MagicMock(side_effect=side_effect)
+            with patch('execute_job.time.sleep') as sleep_mock:
+                sleep_mock.return_value = True
+                self.ej.run(user_slurm_parameters)
+            run_process_mock.assert_any_call("sbatch  -o unit_test_execute_job/unit_test_execute_job.log unit_test_execute_job/batch_job.sh unit_test_execute_job/unit_test_execute_job.x")
+            run_process_mock.assert_any_call("squeue -j  --format %t")
+        if os.path.exists(self.input_file_directory):
+            shutil.rmtree(self.input_file_directory)
+
+
+
+
+
+#'sbatch  -o unit_test_execute_job/unit_test_execute_job.log unit_test_execute_job/batch_job.sh unit_test_execute_job/unit_test_execute_job.x'
 if __name__ == '__main__':
     unittest.main()
 
@@ -96,5 +150,8 @@ if __name__ == '__main__':
                                                             ]
                                                 }
                                               ]
-      
+
+
+combinations :
+{'_id' : 1234 , 'compiler_name': cetus , 'parameters': '' }      
 '''
